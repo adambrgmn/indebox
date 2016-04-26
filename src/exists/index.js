dcc10f62f7eb0efd7955e473a92cb7d62622aec7
Adam Bergman <adam@fransvilhelm.com>
2016-04-26 23:37:12 +0200

    Removing uneccesary files

--- src/exists/index.js
+++
@@ -1,5 +0,0 @@
-import { finder } from '../system';
-
-export default function exists(posix) {
-  return finder.exists(Path(posix));
-}
--- src/notify/index.js
+++
@@ -1,8 +0,0 @@
-import { currentApp } from '../system';
-
-export default function notify(obj) {
-  currentApp.displayNotification(obj.message, {
-    withTitle: obj.title || 'Message',
-    subtitle: obj.subtitle || '',
-  });
-}
